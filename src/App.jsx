9b8b4163e9a7e87557575470da338133eda8d3ad
iamajraj <akmalraj07@gmail.com>
2023-07-30 17:16:14 +0600

    update: rendering visible items

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <main>
+      <Virtual childrenHeight={150}>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((i) => (
+          <div key={i} className="box">
+            {i}
+          </div>
+        ))}
+      </Virtual>
+    </main>
+  );
+}
+
+function Virtual({ children, childrenHeight }) {
+  const [lastIndex, setLastIndex] = useState();
+  const [perScreen, setPerScreen] = useState();
+
+  useEffect(() => {
+    evaluate();
+    window.addEventListener('scroll', evaluate);
+
+    return () => {
+      window.removeEventListener('scroll', evaluate);
+    };
+  }, []);
+
+  function evaluate() {
+    const documentHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+
+    let _last = Math.floor(scrollY / childrenHeight);
+    const childToShow = Math.ceil(documentHeight / childrenHeight);
+
+    setLastIndex(_last);
+    setPerScreen(childToShow);
+
+    console.log('scrollY', _last);
+    console.log('childToShow on screen', childToShow);
+  }
+
+  return <>{children.slice(lastIndex, lastIndex + perScreen)}</>;
+}
+
+export default App;
